572d1a55eef834282e3f4fb990fd49e30226a0ef
Eleazar Carvajal <eleazarcarvajal12@gmail.com>
2024-07-04 23:35:31 -0400

    configuraciones para github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/serverless';
+// import vercel from '@astrojs/vercel/serverless';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://ecarvajal12.github.io',
+	base: '/first-telecom-blog',
 	integrations: [mdx(), sitemap()],
-	output: 'server',
-  	adapter: vercel(),
+	// output: 'server',
+  	// adapter: vercel(),
 });
